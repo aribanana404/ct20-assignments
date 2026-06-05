8e23880fac05b281979c523752fd8dde92ef4c9a
aribanana404 <arianana404@gmail.com>
2026-06-04 23:17:33 -0700

    Add files via upload

---
+++ clouds_and_identity_2026_06_05_06_16_49/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(800, 800);
+  background ('#C5D6D5');
+  
+  textSize(50);
+  //fill('#C5D6D5');
+  text('i think therefore i am;', 180, 400);
+  
+  noFill();
+  strokeWeight(3);
+  rect(160, 350, 500, 80);
+  
+}
+
+function draw() {
+  
+  let funCircle = circle(80, 100, 100);
+  
+  
+  
+  
+}
